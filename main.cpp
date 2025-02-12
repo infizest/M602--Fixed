512f0c99920129d1d2a8e059082ac2141867c0d7
infizest <infzst.ebook@gmail.com>
2025-02-12 10:23:00 +0000

    dataset url unpdated

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 
 int main() {
-    std::string url = "https://example.com/api/data";
+    std::string url = "https://raw.githubusercontent.com/infizest/M602--Fixed/refs/heads/main/owid-co2-data.csv";
     std::string apiData = fetchDataFromAPI(url);
     
     std::string filename = "carbon_data.csv";
